9a748d4d664b30ea51c06c790825fbbb18e0a073
arham777 <arhamawan200@gmail.com>
2024-09-28 21:53:08 +0500

    navbar-assign

---
+++ Assignment-navbar/navbar-project/src/App.jsx
@@ -0,0 +1,21 @@
+import Navbar from './Components/Navbar/navbar'
+import './App.css';
+
+function App() {
+
+  return (
+
+      <>
+      <div className="App">
+        <Navbar/>
+      </div>
+
+      <div>
+      <h1>Navbar Assignment</h1>
+      </div>
+      </>
+  
+  )
+}
+
+export default App
