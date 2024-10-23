a2a483ada415d6f9760a456adfe74a1a7f295f61
70137556 <70137556@uol.pk>
2024-10-23 13:44:27 +0500

    data-useState

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -9,14 +9,15 @@ import CompImg  from './assets/components.png';
 import UiImg  from './assets/jsx-ui.png';
 import StateImg  from './assets/state-mgmt.png';
 
-import { CORE_CONCEPTS } from './data';
+import { CORE_CONCEPTS, EXAMPLES } from './data';
 import TabButton from './components/TabButton/TabButton';
 import { useState } from "react";
 
 
 
 function App() {
-    const [selectedTopic, setSelectedTopic ] = useState('please press any button');
+    // const [selectedTopic, setSelectedTopic ] = useState('please press any button');
+    const [selectedTopic, setSelectedTopic ] = useState('components');
 
   // const xyz = ()=>{
   //   console.log("Hello World-parent");
@@ -69,17 +70,30 @@ function App() {
           <TabButton  >Through Children</TabButton>
           <TabButton title='Through props' ></TabButton>
           <TabButton title='Through props' ></TabButton> */}
-          <TabButton select={()=>{xyz('Through children')}} >Through Children</TabButton>
-          <TabButton select={()=>{xyz('Components')}} >Components</TabButton>
-          <TabButton select={()=>{xyz('Through props')}} title='Through props'></TabButton>
 
           {/* <TabButton select={xyz} >Through Children</TabButton>
           <TabButton select={xyz} title='Through props' ></TabButton>
           <TabButton select={xyz} title='Through props' ></TabButton>
           <TabButton select={xyz} >Components</TabButton> */}
+
+          {/* <TabButton select={()=>{xyz('Through children')}} >Through Children</TabButton>
+          <TabButton select={()=>{xyz('Components')}} >Components</TabButton>
+          <TabButton select={()=>{xyz('Through props')}} title='Through props'></TabButton>  */}
+
+          <TabButton select={()=>{xyz('jsx')}} >JSX</TabButton>
+          <TabButton select={()=>{xyz('components')}} >Components</TabButton>
+          <TabButton select={()=>{xyz('props')}} >Props</TabButton>
+          <TabButton select={()=>{xyz('state')}} title='State'></TabButton>
          
         </menu>
-        {DynamicContent}
+        {/* {DynamicContent} */}
+        <div id='tab-content' >
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+        </div>
         </section>
 
       </main>
